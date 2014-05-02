55cec159a40a94f9d7668a5760bd4b6effb04b8f
Gurps Bassi <gurpiar.bassi@gmail.com>
2014-05-02 23:08:38 +0100

    Builder impl.

---
+++ designpatterns/src/main/java/com/gurps/designpatterns/creational/builder/Person.java
@@ -0,0 +1,84 @@
+package com.gurps.designpatterns.creational.builder;
+
+public class Person {
+
+	private final String firstName;
+	private final String lastName;
+	private final int age;
+	private final String occupation;
+	private final String countryOfBirth;
+
+	/**
+	 * private constructor to prevent instantiation via client code.
+	 * @param personBuilder
+	 */
+	private Person(PersonBuilder personBuilder) {
+		this.firstName = personBuilder.firstName;
+		this.lastName = personBuilder.lastName;
+		this.age = personBuilder.age;
+		this.occupation = personBuilder.occupation;
+		this.countryOfBirth = personBuilder.countryOfBirth;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public String getCountryOfBirth() {
+		return countryOfBirth;
+	}
+
+	public static class PersonBuilder {
+
+		private final String firstName; //required field
+		private final String lastName; //required field
+		private int age;
+		private String occupation;
+		private String countryOfBirth;
+
+		/**
+		 * constructor andles only the required fields.
+		 * @param firstName
+		 * @param lastName
+		 */
+		public PersonBuilder(String firstName, String lastName) {
+
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
+
+		public PersonBuilder age(int age) {
+			this.age = age;
+			return this;
+		}
+
+		public PersonBuilder occupation(String occupation) {
+			this.occupation = occupation;
+			return this;
+
+		}
+
+		public PersonBuilder countryOfBirth(String countyOfBirth) {
+			this.countryOfBirth = countyOfBirth;
+			return this;
+
+		}
+
+		public Person build() {
+			return new Person(this);
+
+		}
+	}
+}
---
+++ designpatterns/src/test/java/com/gurps/designpatterns/creational/builder/TestPerson.java
@@ -0,0 +1,20 @@
+package com.gurps.designpatterns.creational.builder;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPerson {
+
+	@Test
+	public void testPersonBuild() {
+		Person person = new Person.PersonBuilder("John", "Doe").age(10).occupation("Manager").countryOfBirth("Australlia").build();
+		
+		assertEquals("John", person.getFirstName());
+		assertEquals("Doe", person.getLastName());
+		assertEquals("Manager", person.getOccupation());
+		assertEquals(10, person.getAge());
+		assertEquals("Australlia", person.getCountryOfBirth());
+	}
+
+}
